2fac1b2a3a9a438b42d87de3fb59f8493d06ff90
cryptoguru1112 <95309117+cryptoguru1112@users.noreply.github.com>
2022-11-09 03:19:41 +1000

    update

--- js/main.js
+++ js/main.js
@@ -100,7 +100,7 @@ jQuery(document).ready(function ($) {
 			width: 24,
 			rotate: false,
 			image: {
-				url: "images/center.png",
+				url: "https://spinning-wheel-frontend.vercel.app/center.png",
 				width: 24,
 			},
 			class: "wheel-standard-center",
